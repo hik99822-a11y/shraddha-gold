21453687ce53bedd0b796bd37da7bc25cb6c3bb0
hik99822-a11y <hik99822@gmail.com>
2026-09-21 17:07:02 +0530

    UPDATE: vite config file

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,10 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 5173,
-    host: true
+    host: true,
+    allowedHosts: [
+      'shraddhagold.com',
+      'www.shraddhagold.com'
+    ]
   },
+
   build: {
     rollupOptions: {
       output: {
